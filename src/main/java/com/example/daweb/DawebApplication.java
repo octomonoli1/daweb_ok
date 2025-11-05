19f8adc7f1c967e08896cc318be6ed6eb6d4d8e9
vricheb973 <vricheb973@g.educaand.es>
2025-11-05 09:21:48 +0100

    Initial commit

---
+++ src/main/java/com/example/daweb/DawebApplication.java
@@ -0,0 +1,13 @@
+package com.example.daweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawebApplication.class, args);
+	}
+
+}
