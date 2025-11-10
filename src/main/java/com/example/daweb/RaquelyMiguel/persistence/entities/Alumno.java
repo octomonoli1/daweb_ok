7405f6cf9388f9f1612e5f13782d357d4c165417
mgonpor <miguelgp2005@gmail.com>
2025-11-10 10:55:44 +0100

    create Alumno.java

---
+++ src/main/java/com/example/daweb/RaquelyMiguel/persistence/entities/Alumno.java
@@ -0,0 +1,4 @@
+package com.example.daweb.RaquelyMiguel.persistence.entities;
+
+public class Alumno {
+}
--- src/main/java/com/example/daweb/persistence/entities/Alumno.java
+++
@@ -1,4 +0,0 @@
-package com.example.daweb.persistence.entities;
-
-public class Alumno {
-}
