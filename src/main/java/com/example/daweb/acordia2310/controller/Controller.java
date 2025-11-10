6ce26d31b152478d7da2d512c2fcabb60b2fb03d
Andrés José Cortés Díaz <andrescortesdiaz2005@gmail.com>
2025-11-10 11:02:02 +0100

    Subir rama y carpeta

---
+++ src/main/java/com/example/daweb/acordia2310/controller/Controller.java
@@ -0,0 +1,4 @@
+package com.example.daweb.acordia2310.controller;
+
+public class Controller {
+}
---
+++ src/main/java/com/example/daweb/acordia2310/persistence/Repository/Repository.java
@@ -0,0 +1,8 @@
+package com.example.daweb.acordia2310.persistence.Repository;
+
+import com.example.daweb.acordia2310.persistence.Entity.Entity;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface Repository extends ListCrudRepository<Entity, Integer>{
+
+}
