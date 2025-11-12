eb8537843181ae614f3b4e904f523fbdf77d9933
Andrés José Cortés Díaz <andrescortesdiaz2005@gmail.com>
2025-11-12 08:41:55 +0100

    Primer commit montaje de los .java

---
+++ src/main/java/com/example/daweb/acordia2310/services/Exceptions/AlumnoNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.daweb.acordia2310.services.Exceptions;
+
+public class AlumnoNotFoundException extends RuntimeException {
+    public AlumnoNotFoundException(String message) {
+        super(message);
+    }
+}
