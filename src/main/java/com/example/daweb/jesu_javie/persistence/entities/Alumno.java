65a5e2809f5ee7ce5db540faee692b62e36658d1
javiierr13 <fjavierrj6@gmail.com>
2025-11-12 09:26:35 +0100

    setters y getters

--- src/main/java/com/example/daweb/jesu_javie/persistence/entities/Alumno.java
+++ src/main/java/com/example/daweb/jesu_javie/persistence/entities/Alumno.java
@@ -2,8 +2,12 @@ package com.example.daweb.jesu_javie.persistence.entities;
 
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
+@Getter
+@Setter
 
 @Entity
     @Table(name = "alumno")
