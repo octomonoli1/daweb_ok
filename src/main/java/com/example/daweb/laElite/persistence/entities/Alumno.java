1ec7f1240b6aa2955774ddfbd3b7e63665577a5e
jjimho439 <fantitasingas@gmail.com>
2025-11-12 08:51:57 +0100

    Entidad y Service

--- src/main/java/com/example/daweb/laElite/persistence/entities/Alumno.java
+++ src/main/java/com/example/daweb/laElite/persistence/entities/Alumno.java
@@ -1,4 +1,29 @@
 package com.example.daweb.laElite.persistence.entities;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.context.annotation.Primary;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="alumno")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Alumno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 200, nullable = false)
+    private String name;
+
+    @Column(length = 250)
+    private String surname;
+
+    private LocalDate birth;
 }
--- src/main/java/com/example/daweb/laElite/services/AlumnoService.java
+++ src/main/java/com/example/daweb/laElite/services/AlumnoService.java
@@ -1,4 +1,17 @@
 package com.example.daweb.laElite.services;
 
+import com.example.daweb.laElite.persistence.entities.Alumno;
+import com.example.daweb.laElite.persistence.repositories.AlumnoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AlumnoService {
+
+    @Autowired
+    private AlumnoRepository alumnoRepository;
+
+    private Alumno create(Alumno alumno){
+        return alumnoRepository.save(alumno);
+    }
 }
