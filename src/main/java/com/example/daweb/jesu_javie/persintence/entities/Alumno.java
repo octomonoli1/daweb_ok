5b62afc94718620179e015289abf5edf9e667d21
jessuss00 <jesusmadro23@gmail.com>
2025-11-12 08:54:37 +0100

    alumno y correccion de repo

--- src/main/java/com/example/daweb/jesu_javie/persintence/entities/Alumno.java
+++ src/main/java/com/example/daweb/jesu_javie/persintence/entities/Alumno.java
@@ -1,4 +1,23 @@
 package com.example.daweb.jesu_javie.persintence.entities;
 
-public class Alumno {
-}
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+    @Table(name = "alumno")
+    public class Alumno {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Integer id;
+
+        private String name;
+
+        private String surname;
+
+        private LocalDate birth;
+
+
+    }
