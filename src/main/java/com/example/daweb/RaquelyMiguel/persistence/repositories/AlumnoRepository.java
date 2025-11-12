fb55969730bd84b8876043191f9ff2ab7d56e3c2
mgonpor <miguelgp2005@gmail.com>
2025-11-12 08:36:45 +0100

    jpa repository

--- src/main/java/com/example/daweb/RaquelyMiguel/persistence/repositories/AlumnoRepository.java
+++ src/main/java/com/example/daweb/RaquelyMiguel/persistence/repositories/AlumnoRepository.java
@@ -1,7 +1,7 @@
 package com.example.daweb.RaquelyMiguel.persistence.repositories;
 
 import com.example.daweb.RaquelyMiguel.persistence.entities.Alumno;
-import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AlumnoRepository extends ListCrudRepository<Alumno, Integer> {
+public interface AlumnoRepository extends JpaRepository<Alumno, Integer> {
 }
