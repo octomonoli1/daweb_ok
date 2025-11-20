a5672069e43cbca115363a9fbf424e2c6dd2b050
Andrés José Cortés Díaz <andrescortesdiaz2005@gmail.com>
2025-11-20 13:41:22 +0100

    test commit

--- src/test/java/com/example/daweb/acordiaTest/TestFindAll.java
+++ src/test/java/com/example/daweb/acordiaTest/TestFindAll.java
@@ -1,4 +1,24 @@
 package com.example.daweb.acordiaTest;
 
+import com.example.daweb.acordia2310.persistence.Entity.AlumnoEntity;
+import com.example.daweb.acordia2310.persistence.Repository.AlumnoRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
 public class TestFindAll {
+
+    @Test
+    void testFindAll() {
+        AlumnoRepository repo = Mockito.mock(AlumnoRepository.class);
+
+        // Lo que queremos que devuelva
+        Mockito.when(repo.findAll()).thenReturn(List.of(new AlumnoEntity(), new AlumnoEntity()));
+
+        // Comprobación super simple
+        assert repo.findAll().size() == 2;
+    }
 }
