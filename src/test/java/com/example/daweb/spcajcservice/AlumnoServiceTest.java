5d885d9b96653e1b5791ba0491bbb76beeba0a7d
speicon1911 <salvadorpeinado111906@gmail.com>
2025-11-22 10:47:41 +0100

    Update AlumnoServiceTest.java

--- src/test/java/com/example/daweb/spcajcservice/AlumnoServiceTest.java
+++ src/test/java/com/example/daweb/spcajcservice/AlumnoServiceTest.java
@@ -1,12 +1,20 @@
 package com.example.daweb.spcajcservice;
 
+import com.example.daweb.spcajc.persistence.entities.Alumno;
 import com.example.daweb.spcajc.persistence.repositories.AlumnoRepository;
 import com.example.daweb.spcajc.service.AlumnoService;
+import com.example.daweb.spcajc.service.exceptions.AlumnoNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
 public class AlumnoServiceTest {
@@ -19,10 +27,25 @@ public class AlumnoServiceTest {
     @Test
     public void findByName_Ok(){
         // given
+        String name = "Juan";
+        List<Alumno> alumnos = List.of(new Alumno());
+        Mockito.when(alumnoRepository.findByName(name)).thenReturn(alumnos);
 
-        // when
+        // When
+        List<Alumno> result = alumnoService.findByName(name);
 
-        // then
+        // Then
+        Mockito.verify(alumnoRepository).findByName(name);
+        assert(!result.isEmpty());
+    }
 
+    @Test
+    public void findByName_NotFound(){
+        // Given
+        String name = "Pepito";
+        Mockito.when(alumnoRepository.findByName(name)).thenReturn(List.of());
+
+        // When - Then
+        assertThrows(AlumnoNotFoundException.class, () -> alumnoService.findByName(name));
     }
 }
